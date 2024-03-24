a46a3a30edb65415ffd69843ca2c42724132599d
KD5VMF <54245381+KD5VMF@users.noreply.github.com>
2024-03-24 13:45:21 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,8 +0,0 @@
-# This file is where you keep secret settings, passwords, and tokens!
-# If you put them in the code you risk committing that info or sharing it
-# which would be not great. So, instead, keep it all in this one file and
-# keep it a secret.
-
-secrets = {
-   
-    }
